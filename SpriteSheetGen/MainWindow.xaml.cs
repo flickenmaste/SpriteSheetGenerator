19e81d7e93a950442c9ad1770615148512c2fb97
BalthazarAIE <justinl@aie.edu.au>
2014-05-05 16:56:55 -0700

    First Commit
    nice

---
+++ SpriteSheetGen/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpriteSheetGen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyVisualHost visualHost = new MyVisualHost();
+            canvas1.Children.Add(visualHost);
+        }
+
+        private void MenuItemOpen_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = "image"; // Default file name 
+            dlg.DefaultExt = ".png"; // Default file extension 
+            dlg.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png"; // Filter files by extension
+            dlg.Multiselect = true;
+
+            // Show open file dialog box 
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results 
+            if (result == true)
+            {
+                // grab the file name of the image 
+                string[] filename = dlg.FileNames;
+
+                // and load the image 
+
+                canvas1.LoadImage(filename[0]);
+
+            } 
+        } 
+    }
+
+    public class MyVisualHost : FrameworkElement
+    {
+        // Create a collection of child visual objects.
+        private VisualCollection _children;
+
+        public MyVisualHost()
+        {
+            _children = new VisualCollection(this);
+            _children.Add(CreateDrawingVisualRectangle());
+
+        }
+
+        // Create a DrawingVisual that contains a rectangle.
+        private DrawingVisual CreateDrawingVisualRectangle()
+        {
+            DrawingVisual drawingVisual = new DrawingVisual();
+
+            // Retrieve the DrawingContext in order to create new drawing content.
+            DrawingContext drawingContext = drawingVisual.RenderOpen();
+
+            // Create a rectangle and draw it in the DrawingContext.
+            Rect rect = new Rect(new Point(160, 100), new Size(320, 80));
+            drawingContext.DrawRectangle(Brushes.LightBlue, (Pen)null, rect);
+
+            // Persist the drawing content.
+            drawingContext.Close();
+
+            return drawingVisual;
+        }
+    }
+
+
+}
---
+++ SpriteSheetGen/MyCanvas.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace SpriteSheetGen
+{
+    public class MyCanvas : Canvas
+    {
+        BitmapImage background = null;
+
+        public void LoadImage(string filename)
+        {
+            background = new BitmapImage(new Uri(filename));
+            this.InvalidateVisual();
+        }
+
+        public void GenSheet(string[] images)
+        {
+            BitmapFrame[] frames = null;
+            int[] imageWidth = null;
+            int[] imageHeight = null;
+            int iW;
+            int iH;
+            // Load images and get sizes
+            for (int i = 0; i < images.Length; i++)
+            {
+                frames[i] = BitmapDecoder.Create(new Uri(images[i]), BitmapCreateOptions.None, BitmapCacheOption.OnLoad).Frames.First();
+                imageWidth[i] = frames[i].PixelWidth;
+                imageHeight[i] = frames[i].PixelHeight;
+            }
+            iW = frames[0].PixelWidth;
+            iH = frames[0].PixelHeight;
+        }
+
+        protected override void OnRender(DrawingContext dc)
+        {
+            if (background != null)
+            {
+                dc.DrawImage(background, new Rect(0, 0, background.PixelWidth,
+               background.PixelHeight));
+            }
+        }
+    }
+}
